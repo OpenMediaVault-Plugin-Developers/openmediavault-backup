895d70b23fad3956e91f11757856be13ec805097
root <root@localhost.localdomain>
2013-11-22 19:25:49 -0600

    initial commit

---
+++ var/www/openmediavault/js/omv/module/admin/system/backup/Settings.js
@@ -0,0 +1,123 @@
+/**
+ * Copyright (C)      2013 OpenMediaVault Plugin Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/form/Panel.js")
+// require("js/omv/workspace/window/Form.js")
+// require("js/omv/data/Store.js")
+// require("js/omv/data/Model.js")
+// require("js/omv/data/proxy/Rpc.js")
+// require("js/omv/workspace/window/plugin/ConfigObject.js")
+// require("js/omv/form/field/SharedFolderComboBox.js")
+
+Ext.define("OMV.module.admin.system.backup.Settings", {
+    extend : "OMV.workspace.form.Panel",
+    uses   : [
+        "OMV.data.Model",
+        "OMV.data.Store"
+    ],
+
+    rpcService   : "Backup",
+    rpcGetMethod : "getSettings",
+    rpcSetMethod : "setSettings",
+
+    getFormItems : function() {
+        return [{
+            xtype    : "fieldset",
+            title    : _("Settings"),
+            defaults : {
+                labelSeparator : ""
+            },
+            items : [{
+                xtype         : "combo",
+                name          : "mntentref",
+                fieldLabel    : _("Backup Volume"),
+                emptyText     : _("Select a volume ..."),
+                allowBlank    : false,
+                allowNone     : false,
+                editable      : false,
+                triggerAction : "all",
+                displayField  : "description",
+                valueField    : "uuid",
+                store         : Ext.create("OMV.data.Store", {
+                    autoLoad : true,
+                    model    : OMV.data.Model.createImplicit({
+                        idProperty : "uuid",
+                        fields     : [
+                            { name : "uuid", type : "string" },
+                            { name : "devicefile", type : "string" },
+                            { name : "description", type : "string" }
+                        ]
+                    }),
+                    proxy : {
+                        type : "rpc",
+                        rpcData : {
+                            service : "Backup",
+                            method  : "getCandidates"
+                        },
+                        appendSortParams : false
+                    },
+                    sorters : [{
+                        direction : "ASC",
+                        property  : "devicefile"
+                    }]
+                })
+            },{
+                xtype      : "textfield",
+                name       : "path",
+                fieldLabel : _("Backup Path"),
+                allowNone  : true,
+                readOnly   : true
+            },{
+                border : false,
+                html   : _("Backup the operating system drive to a data drive for emergency restoration.") + "<br /><br />"
+            },{
+                xtype   : "button",
+                name    : "backup",
+                text    : _("Backup"),
+                scope   : this,
+                handler : Ext.Function.bind(me.onBackupButton, me, [ me ])
+            },{
+                border : false,
+                    html   : "<br />"
+            }]
+        }];
+    },
+
+    onBackupButton: function() {
+        var me = this;
+        Ext.create("OMV.window.Execute", {
+            title      : _("Backup"),
+            rpcService : "Backup",
+            rpcMethod  : "executeBackup",
+            listeners  : {
+                scope     : me,
+                exception : function(wnd, error) {
+                    OMV.MessageBox.error(null, error);
+                }
+            }
+        }).show();
+    }
+});
+
+OMV.WorkspaceManager.registerPanel({
+    id        : "settings",
+    path      : "/system/backup",
+    text      : _("Settings"),
+    position  : 10,
+    className : "OMV.module.admin.system.backup.Settings"
+});
