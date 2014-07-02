64063fd1289678c598dd23f1a0a36041b0a1dd6a
Aaron Murray <plugins@omv-extras.org>
2014-07-02 07:46:55 -0500

    add clonezilla boot

--- var/www/openmediavault/js/omv/module/admin/system/backup/SystemBackup.js
+++ var/www/openmediavault/js/omv/module/admin/system/backup/SystemBackup.js
@@ -95,6 +95,48 @@ Ext.define("OMV.module.admin.system.backup.SystemBackup", {
                 border : false,
                 html   : "<ul><li>" + _("Backup the operating system drive to a data drive for emergency restoration.") + "</li></ul>"
             }]
+        },{
+            xtype    : "fieldset",
+            title    : _("Clonezilla"),
+            defaults : {
+                labelSeparator : ""
+            },
+            items : [{
+                xtype   : "button",
+                name    : "install",
+                text    : _("Install"),
+                scope   : this,
+                handler : Ext.Function.bind(me.onInstallButton, me, [ me ]),
+                margin  : "5 0 0 0"
+            },{
+                border : false,
+                html   : "<ul>" +
+                         "<li>" + _("Downloads Clonezilla ISO and configures grub bootloader to allow booting from ISO.") + "</li>" +
+                         "<li>" + _("SSH server is enabled by default.  Login with username: <b>user</b> and password: <b>live</b>") + "</li>" +
+                         "<li>" + _("IP Address will be set by DHCP.") + "</li>" +
+                         "<li>" + _("When logging in remotely, start clonezilla with:  sudo clonezilla") + "</li>" +
+                         "</ul>"
+            },{
+                xtype   : "button",
+                name    : "rebootcz",
+                text    : _("Clonezilla"),
+                scope   : this,
+                handler : Ext.Function.bind(me.onCzButton, me, [ me ]),
+                margin  : "5 0 0 0"
+            },{
+                border : false,
+                html   : "<ul><li>" + _("Sets grub bootloader to boot from Clonezilla ISO <b>ONE</b> time.") + "</li></ul>"
+            },{
+                xtype   : "button",
+                name    : "rebootomv",
+                text    : _("OMV"),
+                scope   : this,
+                handler : Ext.Function.bind(me.onOmvButton, me, [ me ]),
+                margin  : "5 0 0 0"
+            },{
+                border : false,
+                html   : "<ul><li>" + _("Sets grub bootloader to boot normally from OpenMediaVault.") + "</li></ul>"
+            }]
         }];
     },
 
@@ -112,7 +154,54 @@ Ext.define("OMV.module.admin.system.backup.SystemBackup", {
                 }
             }
         }).show();
+    },
+
+    onInstallButton: function() {
+        var me = this;
+        me.doSubmit();
+        Ext.create("OMV.window.Execute", {
+            title      : _("Install Clonezilla"),
+            rpcService : "Backup",
+            rpcMethod  : "doInstall",
+            listeners  : {
+                scope     : me,
+                exception : function(wnd, error) {
+                    OMV.MessageBox.error(null, error);
+                }
+            }
+        }).show();
+    },
+
+    onCzButton: function() {
+        var me = this;
+        Ext.create("OMV.window.Execute", {
+            title      : _("Reboot to Clonezilla"),
+            rpcService : "Backup",
+            rpcMethod  : "doRebootClonezilla",
+            listeners  : {
+                scope     : me,
+                exception : function(wnd, error) {
+                    OMV.MessageBox.error(null, error);
+                }
+            }
+        }).show();
+    },
+
+    onOmvButton: function() {
+        var me = this;
+        Ext.create("OMV.window.Execute", {
+            title      : _("Reboot to OpenMediaVault"),
+            rpcService : "Backup",
+            rpcMethod  : "doRebootOMV",
+            listeners  : {
+                scope     : me,
+                exception : function(wnd, error) {
+                    OMV.MessageBox.error(null, error);
+                }
+            }
+        }).show();
     }
+
 });
 
 OMV.WorkspaceManager.registerPanel({
