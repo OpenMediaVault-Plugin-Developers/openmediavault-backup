f402e3afb3f8e9cca4401f975790691430bd25c8
Aaron Murray <plugins@omv-extras.org>
2016-01-03 11:32:31 -0600

    Adapt for OMV 3.0

--- var/www/openmediavault/js/omv/module/admin/system/backup/SystemBackup.js
+++ var/www/openmediavault/js/omv/module/admin/system/backup/SystemBackup.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2013-2014 OpenMediaVault Plugin Developers
+ * Copyright (C) 2013-2016 OpenMediaVault Plugin Developers
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
