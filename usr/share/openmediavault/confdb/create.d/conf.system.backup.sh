28e74fa3e7b1bcaf1bc23b8103bf0a1f58dfc8d5
Aaron Murray <plugins@omv-extras.org>
2021-12-11 11:06:39 -0600

    add scheduled task

--- usr/share/openmediavault/confdb/create.d/conf.system.backup.sh
+++ usr/share/openmediavault/confdb/create.d/conf.system.backup.sh
@@ -4,7 +4,7 @@
 # @author    Volker Theile <volker.theile@openmediavault.org>
 # @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
 # @copyright Copyright (c) 2009-2013 Volker Theile
-# @copyright Copyright (c) 2013-2020 OpenMediaVault Plugin Developers
+# @copyright Copyright (c) 2013-2021 OpenMediaVault Plugin Developers
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -34,6 +34,26 @@ if ! omv_config_exists "${SERVICE_XPATH}"; then
     omv_config_add_key "${SERVICE_XPATH}" "extraoptions" ""
     omv_config_add_key "${SERVICE_XPATH}" "keep" "7"
     omv_config_add_key "${SERVICE_XPATH}" "passwd" ""
+fi
+
+if ! omv_config_exists "//system/crontab/job[uuid='32664b22-5a9d-11ec-8834-6f00f75b23ee']"; then
+    object="<uuid>32664b22-5a9d-11ec-8834-6f00f75b23ee</uuid>"
+    object="${object}<enable>0</enable>"
+    object="${object}<execution>exactly</execution>"
+    object="${object}<sendemail>0</sendemail>"
+    object="${object}<comment></comment>"
+    object="${object}<type>userdefined</type>"
+    object="${object}<minute>30</minute>"
+    object="${object}<everynminute>0</everynminute>"
+    object="${object}<hour>1</hour>"
+    object="${object}<everynhour>0</everynhour>"
+    object="${object}<month>*</month>"
+    object="${object}<dayofmonth>*</dayofmonth>"
+    object="${object}<everyndayofmonth>0</everyndayofmonth>"
+    object="${object}<dayofweek>7</dayofweek>"
+    object="${object}<username>root</username>"
+    object="${object}<command>/usr/sbin/omv-backup</command>"
+    omv_config_add_node_data "//system/crontab" "job" "${object}"
 fi
 
 exit 0
