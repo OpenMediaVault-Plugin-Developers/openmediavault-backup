b9c4191b2199b44fd4a73470069029d3627adfad
Aaron Murray <plugins@omv-extras.org>
2022-07-17 12:45:49 -0500

    add button to restore cron task

--- usr/share/openmediavault/confdb/create.d/conf.system.backup.sh
+++ usr/share/openmediavault/confdb/create.d/conf.system.backup.sh
@@ -36,24 +36,7 @@ if ! omv_config_exists "${SERVICE_XPATH}"; then
     omv_config_add_key "${SERVICE_XPATH}" "passwd" ""
 fi
 
-if ! omv_config_exists "//system/crontab/job[uuid='32664b22-5a9d-11ec-8834-6f00f75b23ee']"; then
-    object="<uuid>32664b22-5a9d-11ec-8834-6f00f75b23ee</uuid>"
-    object="${object}<enable>0</enable>"
-    object="${object}<execution>exactly</execution>"
-    object="${object}<sendemail>0</sendemail>"
-    object="${object}<comment></comment>"
-    object="${object}<type>userdefined</type>"
-    object="${object}<minute>30</minute>"
-    object="${object}<everynminute>0</everynminute>"
-    object="${object}<hour>1</hour>"
-    object="${object}<everynhour>0</everynhour>"
-    object="${object}<month>*</month>"
-    object="${object}<dayofmonth>*</dayofmonth>"
-    object="${object}<everyndayofmonth>0</everyndayofmonth>"
-    object="${object}<dayofweek>7</dayofweek>"
-    object="${object}<username>root</username>"
-    object="${object}<command>/usr/sbin/omv-backup</command>"
-    omv_config_add_node_data "//system/crontab" "job" "${object}"
-fi
+# add or re-add cron job
+omv-backup-fix-cron
 
 exit 0
